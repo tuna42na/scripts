992f700a958bc2400dc3f0b8fb23cbd4cc63d92e
Tuna Fortuna <tuna.fortuna@groundfloor.us>
2024-03-19 17:17:07 -0400

    feat[2NA]: add conventional commits

--- convential-commit.sh
+++ convential-commit.sh
@@ -2,13 +2,17 @@
 # This script is used to create a conventional commit message with gum
 
 # VARIABLES
+echo 'Commit Type:'
 ACTION=$(gum choose 'feat' 'fix' 'docs' 'style' 'refactor' 'perf' 'test' 'build' 'ci' 'chore' 'revert')
 PR=$(gum input --placeholder 'Enter the PR number')
 MESSAGE=$(gum input --placeholder 'Enter the commit message')
-BREAKING_CHANGE=$(gum confirm 'Is this a breaking change?')
-if [ "$BREAKING_CHANGE" = "yes" ]; then
+echo 'Breaking change?:'
+BREAKING_CHANGE=$(gum choose 'nope' 'yup')
+if [ "$BREAKING_CHANGE" = "yup" ]; then
   BANG="!"
+else
+  BANG=""
 fi
 
 # COMMIT MESSAGE
-git commit -m "$ACTION[$PR]: $MESSAGE" -m "BREAKING CHANGE: $BREAKING_CHANGE"
+git commit -m "$BANG$ACTION[$PR]: $MESSAGE"
