733964f03e25b075e23bec8f6374500054aa4307
Tuna Fortuna <tuna.fortuna@groundfloor.us>
2024-07-30 17:03:40 -0400

    feat[2NA]: tidy up files

--- alias.sh
+++ alias.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SCRIPT_DIR=$(pwd)
-alias gcc='$SCRIPT_DIR/convential-commit.sh'
-alias gpr='$SCRIPT_DIR/create-pr.sh'
-alias smk='$SCRIPT_DIR/smokes.sh'
-alias gst='$SCRIPT_DIR/stash-message.sh'
+CURRENT_DIRECTORY=$(pwd)
+alias gcc='$CURRENT_DIRECTORY/src/convential-commit.sh'
+alias gpr='$CURRENT_DIRECTORY/src/create-pr.sh'
+alias smk='$CURRENT_DIRECTORY/src/smokes.sh'
+alias gst='$CURRENT_DIRECTORY/src/stash-message.sh'
