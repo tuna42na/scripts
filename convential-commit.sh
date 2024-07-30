605462f75f14f099197f36f3dd094474513707cf
Tuna Fortuna <tuna.fortuna@groundfloor.us>
2024-07-30 16:54:19 -0400

    chore[2NA]: update commit message to get team name

--- convential-commit.sh
+++ convential-commit.sh
@@ -6,7 +6,13 @@ ACTION=$(gum filter --placeholder "Commit type:" 'build' 'chore' 'ci' 'docs' 'fe
 tput cuu1
 tput el
 echo "Commit type: $ACTION ✅"
-PR=$(gum input --prompt "PR Number: " --placeholder 'ex. CL-*** || SB-***')
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+TEAM=$(echo $BRANCH | cut -d '-' -f1)
+PR_NUMBER=$(echo $BRANCH | cut -d'-' -f2)
+PR=$(gum choose "$TEAM-$PR_NUMBER" "other")
+if [ "$PR" = "other" ]; then
+  PR=$(gum input --prompt "PR Number: " --placeholder "$TEAM-$PR_NUMBER")
+fi
 echo "PR: $PR ✅"
 MESSAGE=$(gum input --prompt "Commit message: " --placeholder 'added a commit message...')
 echo "Commit message: $MESSAGE ✅"
