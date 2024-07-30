d4d800e86bfa44647db9d68f403ce8bdc1919bb0
Tuna Fortuna <tuna.fortuna@groundfloor.us>
2024-07-30 17:12:02 -0400

    chore[2NA]: update naming for mobile branch clean

--- alias.sh
+++ alias.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 CURRENT_DIRECTORY=$(pwd)
-alias gcc='$CURRENT_DIRECTORY/src/convential-commit.sh'
+alias cmb='$CURRENT_DIRECTORY/src/clean-mobile-branch.sh'
+alias gcc='$CURRENT_DIRECTORY/src/conventional-commit.sh'
 alias gpr='$CURRENT_DIRECTORY/src/create-pr.sh'
 alias smk='$CURRENT_DIRECTORY/src/smokes.sh'
 alias gst='$CURRENT_DIRECTORY/src/stash-message.sh'
